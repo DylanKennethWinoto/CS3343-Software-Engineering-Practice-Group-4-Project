08b06472ee4fd7bd3a3cdb8cfee3ba24fad5face
ZHANG Fanglu <fzhang279-c@my.cityu.edu.hk>
2024-11-24 15:53:30 +0800

    FinancialReportGenerator.java

--- company_financial_analyzer/src/company_financial_analyzer/FinancialReportGenerator.java
+++ company_financial_analyzer/src/company_financial_analyzer/FinancialReportGenerator.java
@@ -240,7 +240,6 @@ public class FinancialReportGenerator {
 		}
 	}
 	public static void showinput () {
-	
         System.out.println("MM/YYYY (e.g., Jan/2024)");
         System.out.println("Q1/YYYY (e.g., Q1/2024)");
         System.out.println("YYYY (e.g., 2024)");
