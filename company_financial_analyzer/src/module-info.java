0e9076fdfd8a1df6c2bc29368f08c13b7db35bc0
WANG Shenglin <swang586-c@my.cityu.edu.hk>
2024-11-28 14:29:46 +0800

    Update module-info.java

--- company_financial_analyzer/src/module-info.java
+++ company_financial_analyzer/src/module-info.java
@@ -6,6 +6,8 @@
  */
 module company_financial_analyzer {
     exports company_financial_analyzer;
-    exports financial_analyzer_test; // 确保导出测试包
+    exports financial_analyzer_test; 
 	requires junit;
+	requires java.desktop;
+
 }
